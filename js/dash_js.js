2a3dc727ca096d8b9815d8f5fc05ac6f86d52f6e
EB <eddyojb@gmail.com>
2018-09-08 12:50:50 +0100

    init

---
+++ js/dash_js.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//         // var firstDivContent = document.getElementById('hidden-1');
+//         // var secondDivContent = document.getElementById('help-1');
+//         //
+//         // secondDivContent.innerHTML = firstDivContent.innerHTML;
+//         alert("I have loaded")
+// });
+//
+// $(function () {
+//   $('[data-toggle="popover"]').popover({
+//       container: '#dashboard-inner-container'
+//   });
+// })
+
+alert("I am an alert box!");
